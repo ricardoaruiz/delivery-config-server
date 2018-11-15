564b9beb5da5921234cbf82d01ef529831927e6e
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2018-11-14 23:50:04 -0200

    Criação do Projeto

---
+++ src/main/java/com/rar/delivery/DeliveryConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.rar.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class DeliveryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigServerApplication.class, args);
+	}
+}
